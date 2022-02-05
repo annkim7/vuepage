b881fb310915db019ced83e680e401115c2a0d22
annkim7@github.com <ilc7748@gmail.com>
2022-02-05 17:59:13 +0900

    footer style

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,17 +3,29 @@
         <h1 class="logo"><img src="../assets/logo.png"/></h1>
         <address class="footer-box">
             <div class="footer">
-
+                <div class="privacy">
+                    <ul class="list bar">
+                        <li>서비스안내</li>
+                        <li>고객센터</li>
+                        <li>서비스약관 및 정책</li>
+                        <li>개인정보처리방침</li>
+                    </ul>
+                </div>
+                <div class="infomation">
+                    <ul class="list">
+                        <li>주식회사 회사명</li>
+                        <li>서울시 강남구 강남대로 0길 00-0</li>
+                        <li>대표자 대표자</li>
+                        <li>개인정보관리책임자 책임자</li>
+                        <li>사업자등록번호 000-00-00000</li>
+                        <li>TEL 00-0000-0000</li>
+                        <li>FAX 00-0000-0000</li>
+                        <li>E-MAIL 111@gmail.com</li>
+                        <li>Copyright Ann Kim All Rights Reserved</li>
+                    </ul>
+                </div>
             </div>
-            <ul class="info-list">
-                <li class="name">주식회사 회사명</li>
-                <li class="address">서울시 강남구 강남대로</li>
-                <li class="regiNum">000-00-00000</li>
-                <li class="tel">TEL 00-0000-0000</li>
-                <li class="fax">FAX 00-0000-0000</li>
-                <li class="mail">E-MAIL 111@gmail.com</li>
-                <li class="copyright">Copyright Ann Kim All Rights Reserved</li>
-            </ul>
+            
         </address>
     </footer>
 </template>
@@ -27,5 +39,19 @@ export default {
 
 <style lang="scss" >
 @import '../assets/style/root.scss';
+
+.footer-wrap{
+    display:flex;
+}
+
+.footer{
+    display:flex;
+    flex-flow: row wrap;
+}
+
+.privacy{
+    width:100%;
+}
+
 
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,21 +1,8 @@
 <template>
   <div class="wave">
-    <svg class="editorial" viewbox="0 24 150 28" preserveaspectratio="none">
-        <defs>
-            <path id="gentle-wave"
-            d="M-160 44c30 0 
-            58-18 88-18s
-            58 18 88 18
-            58-18 88-18
-            58 18 88 18" />
-        </defs>
-        <g class="parallax" fill="none" >
-            <use xlink:href="#gentle-wave" x="50" y="0" stroke="#4579e2"/>
-            <use xlink:href="#gentle-wave" x="50" y="3" stroke="#3461c1"/>
-            <use xlink:href="#gentle-wave" x="50" y="6" stroke="#2d55aa"/>
-        </g>
-    </svg>
+    test
   </div>
+  
 </template>
 
 <script>
@@ -28,16 +15,6 @@ export default {
 <style lang="scss" >
 @import '../assets/style/root.scss';
 
-.wave{overflow:hidden; max-height:100vh; margin:0; padding:9em 0 0 0; background:#234;}
-.editorial{display:block;width:100%;height:10em;max-height:100vh; border:1px solid red;}
 
-        // .parallax > use{animation: move-forever 12s linear infinite;}
-        // .parallax > use:nth-child(1){animation-delay: -2s;}
-        // .parallax > use:nth-child(2){animation-delay: -2s;animation-duration: 5s;}
-        // .parallax > use:nth-child(3){animation-delay: -4s;animation-duration: 3s;}
-        // @keyframes move-forever{
-        //     0%{transform: translate(-90px, 0%);}
-        //     100%{transform: translate(85px, 0%);}
-        // }
 
 </style>
