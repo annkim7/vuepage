84fe1dbf344433f562bcd4b856cb8462b312a837
annkim7@github.com <ilc7748@gmail.com>
2022-02-11 11:37:18 +0900

    add sine wave

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,16 +1,20 @@
 <template>
+
   <div class="container">
     <section class="section" :class="{ active: this.pageNum == 0 }">
-      <div class="bg">
-          <svg id="wave" style="transform:rotate(180deg); transition: 0.3s" viewBox="0 0 1440 490">
-            <path style="transform:translate(0, 0px); opacity:1" fill="#5A5A5A" d="M0,392L60,343C120,294,240,196,360,155.2C480,114,600,131,720,171.5C840,212,960,278,1080,269.5C1200,261,1320,180,1440,163.3C1560,147,1680,196,1800,187.8C1920,180,2040,114,2160,89.8C2280,65,2400,82,2520,81.7C2640,82,2760,65,2880,65.3C3000,65,3120,82,3240,130.7C3360,180,3480,261,3600,285.8C3720,310,3840,278,3960,245C4080,212,4200,180,4320,212.3C4440,245,4560,343,4680,318.5C4800,294,4920,147,5040,89.8C5160,33,5280,65,5400,130.7C5520,196,5640,294,5760,302.2C5880,310,6000,229,6120,220.5C6240,212,6360,278,6480,285.8C6600,294,6720,245,6840,220.5C6960,196,7080,196,7200,163.3C7320,131,7440,65,7560,49C7680,33,7800,65,7920,89.8C8040,114,8160,131,8280,138.8C8400,147,8520,147,8580,147L8640,147L8640,490L8580,490C8520,490,8400,490,8280,490C8160,490,8040,490,7920,490C7800,490,7680,490,7560,490C7440,490,7320,490,7200,490C7080,490,6960,490,6840,490C6720,490,6600,490,6480,490C6360,490,6240,490,6120,490C6000,490,5880,490,5760,490C5640,490,5520,490,5400,490C5280,490,5160,490,5040,490C4920,490,4800,490,4680,490C4560,490,4440,490,4320,490C4200,490,4080,490,3960,490C3840,490,3720,490,3600,490C3480,490,3360,490,3240,490C3120,490,3000,490,2880,490C2760,490,2640,490,2520,490C2400,490,2280,490,2160,490C2040,490,1920,490,1800,490C1680,490,1560,490,1440,490C1320,490,1200,490,1080,490C960,490,840,490,720,490C600,490,480,490,360,490C240,490,120,490,60,490L0,490Z"></path>
+      <div class="bg"> 
+          <svg>  
+            <g>    
+              <!-- <line id="line" x1="0" x2="100%" /> -->
+              <polyline class="wave" />
+            </g>  
           </svg>
       </div>
       <div class="inner-wrap">
-        <div class="content">
+        <!-- <div class="content">
           <h2 class="top-title">Lorem Ipsum</h2>
           <p>Lorem Ipsum is simply dummy<br>text of the printing</p>
-        </div>
+        </div> -->
       </div>
     </section>
     <section class="section" :class="{ active: this.pageNum == 1 }">
@@ -43,10 +47,18 @@
       </div>
     </section>
   </div>
+
+
+  <!-- <svg id="wave" style="transform:rotate(180deg); transition: 0.3s" viewBox="0 0 1440 490">
+            <path style="transform:translate(0, 0px); opacity:1" fill="#5A5A5A" d="M0,392L60,343C120,294,240,196,360,155.2C480,114,600,131,720,171.5C840,212,960,278,1080,269.5C1200,261,1320,180,1440,163.3C1560,147,1680,196,1800,187.8C1920,180,2040,114,2160,89.8C2280,65,2400,82,2520,81.7C2640,82,2760,65,2880,65.3C3000,65,3120,82,3240,130.7C3360,180,3480,261,3600,285.8C3720,310,3840,278,3960,245C4080,212,4200,180,4320,212.3C4440,245,4560,343,4680,318.5C4800,294,4920,147,5040,89.8C5160,33,5280,65,5400,130.7C5520,196,5640,294,5760,302.2C5880,310,6000,229,6120,220.5C6240,212,6360,278,6480,285.8C6600,294,6720,245,6840,220.5C6960,196,7080,196,7200,163.3C7320,131,7440,65,7560,49C7680,33,7800,65,7920,89.8C8040,114,8160,131,8280,138.8C8400,147,8520,147,8580,147L8640,147L8640,490L8580,490C8520,490,8400,490,8280,490C8160,490,8040,490,7920,490C7800,490,7680,490,7560,490C7440,490,7320,490,7200,490C7080,490,6960,490,6840,490C6720,490,6600,490,6480,490C6360,490,6240,490,6120,490C6000,490,5880,490,5760,490C5640,490,5520,490,5400,490C5280,490,5160,490,5040,490C4920,490,4800,490,4680,490C4560,490,4440,490,4320,490C4200,490,4080,490,3960,490C3840,490,3720,490,3600,490C3480,490,3360,490,3240,490C3120,490,3000,490,2880,490C2760,490,2640,490,2520,490C2400,490,2280,490,2160,490C2040,490,1920,490,1800,490C1680,490,1560,490,1440,490C1320,490,1200,490,1080,490C960,490,840,490,720,490C600,490,480,490,360,490C240,490,120,490,60,490L0,490Z"></path>
+          </svg> -->
 </template>
 
 <script>
 import { gsap } from "gsap";
+import { CustomEase } from "gsap/CustomEase";
+
+gsap.registerPlugin(CustomEase);
 
 export default {
     name : 'Main',
@@ -58,13 +70,49 @@ export default {
       }
     },
     mounted(){
-      document.addEventListener('scroll', this.scroll, true)
+      document.addEventListener('scroll', this.scroll, true);
+      this.waveBg();
+      
     },
     unmounted() {
       document.removeEventListener('scroll', this.scroll, true);
     },
     methods:{
-      
+      waveBg(){
+          var svg  = document.querySelector("svg");
+          var wave = document.querySelector(".wave");
+
+          var width = window.innerWidth;
+          var sinus = CustomEase.create("sinus", "M0,0 C0.4,0 0.3,1 0.5,1 0.7,1 0.6,0 1,0");
+
+          var amplitude = 150;
+          var frequency = 30;
+          var segments  = 1000;
+          var interval  = width / segments;
+          gsap.defaults({
+            ease: "sine.inOut"
+          });
+
+          gsap.set("g", {
+            y: window.innerHeight / 2
+          });
+
+          for (var i = 0; i <= segments; i++) {
+            
+            var norm = 1 - i / segments;
+            var point = wave.points.appendItem(svg.createSVGPoint());
+            
+            point.x = i * interval;
+            point.y = amplitude / 2 * sinus(norm);
+              
+            gsap.to(point, 0.3, { 
+              duration: 0.3,
+              y: -point.y, 
+              repeat: -1, 
+              yoyo: true 
+            }).progress(norm * frequency);  
+          }
+      },
       scroll(e){
         this.scrollTop = e.target.scrollTop;
         const section = document.getElementsByTagName("section");
@@ -85,6 +133,7 @@ export default {
       pageChangeFunc(){
           const inner = document.querySelectorAll(".inner-wrap");
           const el = inner[this.pageNum];
+          CustomEase.create("custom", "M0,0 C0.266,0.412 0.571,0.079 0.7,0.2 0.744,0.241 0.78,1 1,1 ");
           // el.style.opacity = 0;
           // el.style.transform = 'translateY(-100%)';
           for(var i=0; i<inner.length; i++){
@@ -93,7 +142,8 @@ export default {
                 duration: 1,
                 transform: 'translateY(-100%)',
                 opacity: 0,
-                ease : 'bounce.out',
+                ease : "custom",
+                
               })
           }
 
@@ -101,11 +151,13 @@ export default {
             duration: 1,
             transform: 'translateY(0%)',
             opacity: 1,
-            ease : 'bounce.out',
+            ease : "custom",
           })
-
           
-        }
+          
+      }
+
+
     }
 
 }
@@ -127,10 +179,13 @@ section {
 }
 
 .bg{
-  position: absolute;
-  width:100%;
-  top:0;
-  left:0;
+  // position: absolute;
+  // width:100%;
+  // height:100%;
+  // top:0;
+  // left:0;
+  @include center;
+  height:100%;
 }
 
 .inner-wrap{
@@ -194,4 +249,24 @@ section {
 .half-box .txt{
   width: 50%;
 }
+
+svg {
+  width:100%;
+  margin: 0 auto;
+  height: 100%;
+}
+
+line {
+  stroke-width: 1;
+  stroke: #3c3c3c;
+}
+
+.wave {
+  fill: none;
+  stroke-width: 4;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  stroke: var(--default-color);;
+}
+
 </style>
