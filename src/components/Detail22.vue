797e526e3e29d2fc4d414a6af72131a660402053
annkim7@github.com <ilc7748@gmail.com>
2022-03-18 21:33:23 +0900

    qna style

--- src/components/Detail22.vue
+++ src/components/Detail22.vue
@@ -28,11 +28,31 @@ export default {
         return{
             slideUp : [],
             qna : [
-                {"id": "0", "question" : "질문있어요", "answer" : "답해주세요"},
-                {"id": "1", "question" : "질문있어요1", "answer" : "답해주세요1"},
-                {"id": "2", "question" : "질문있어요2", "answer" : "답해주세요2"},
-                {"id": "3", "question" : "질문있어요3", "answer" : "답해주세요3"},
-                {"id": "4", "question" : "질문있어요4", "answer" : "답해주세요4"},
+                {
+                    "id": "0",
+                    "question" : "내 정보를 수정하고 싶어요. 어디서 해야 하나요?",
+                    "answer" : "마이페이지에 접속 후 원하시는 항목을 클릭하여 수정하시면 됩니다"
+                },
+                {
+                    "id": "1",
+                    "question" : "프로그램은 유료인가요?",
+                    "answer" : "각 프로그램 별 유/무료 참가 비용이 기재되어있습니다."
+                },
+                {
+                    "id": "2",
+                    "question" : "통역신청방법",
+                    "answer" : "통역 지원 가능 여부 및 통역사 배정은 사무국에서 순차 안내드립니다."
+                },
+                {
+                    "id": "3",
+                    "question" : "영수증요청합니다",
+                    "answer" : "운동장 이용방법 문의, 입금확인, 예약취소 및 사용료 반환 요청 등 운동장 이용에 관한 문의 및 답변 게시판입니다."
+                },
+                {
+                    "id": "4",
+                    "question" : "예약취소 요청드립니다.",
+                    "answer" : "우리는 이와 같은 고객과의 약속을 반드시 지키기 위한 구체적인 이행표준을 마련하고, 이를 성실히 준수하겠습니다."
+                },
             ],
         }
     },
@@ -45,22 +65,26 @@ export default {
     },
     methods:{
         toggle(index){
-            let test = this.slideUp.map(e => {
+            let openArr = this.slideUp.map(e => {
                 e = false;
                 return e;
             })
-            this.slideUp = [...test];
-            this.slideUp[index] = !this.slideUp[index];
+            if(this.slideUp[index] == false){
+                this.slideUp = [...openArr];
+                this.slideUp[index] = true;
+            }else{
+                this.slideUp = [...openArr];
+            }
         },
         beforeEnter(el) {
             el.style.height = '0rem';
             el.style.opacity = '0';
             el.style.transitionProperty = 'all';
-            el.style.transitionDuration = '1s';
+            el.style.transitionDuration = '0.6s';
         },
         enter(el, done) {
             setTimeout(()=>{
-                el.style.height = '5rem';
+                el.style.height = '4rem';
                 el.style.opacity = '1';
             }, 100);
             done();
@@ -82,16 +106,38 @@ export default {
 }
 
 .fnq-list{
-    li{
-        span{
-            overflow: hidden;
-            height:5rem;
-            background:#f4f4f4;
-        }
-        .a{
-            background:#ddd;
+    border-top: 1px solid #0e0e0e;
+    border-bottom: 1px solid #0e0e0e;
+    li ~ li{
+        border-top: 1px solid #D1D1D1;
+    }
+}
+
+.fnq-list li{
+    span{
+        @include centerVertical;
+        overflow: hidden;
+        width:100%;
+        height:4rem;
+        padding: 0 1.8rem;
+    }
+    .q{
+        position: relative;
+        &:before{
+            @include bef;
+            left:auto;
+            top: 50%;
+            right:1.8rem;
+            width:0.4rem;
+            height:0.667rem;
+            background: url('../assets/icon_arrow_gr.png') no-repeat center center/cover;
+            transform: translateY(-50%) rotate(90deg);
         }
     }
+    .a{
+        background:#f3f3f3;
+    }
+    
 }
 
 
