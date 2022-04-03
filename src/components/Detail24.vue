79e1e2457fc5d4b780a1f215b3d408cf43bdbd7e
annkim7@github.com <ilc7748@gmail.com>
2022-04-03 14:20:26 +0900

    question style

--- src/components/Detail24.vue
+++ src/components/Detail24.vue
@@ -2,7 +2,27 @@
     <div class="question-area">
         <div class="question-wrap">
             <div class="question-box">
-                
+                <div class="write">
+                    <strong>From.</strong>
+                    <input type="text" name="search-input" placeholder="이름">
+                </div>
+                <div class="write">
+                    <strong>Position.</strong>
+                    <ul class="radio-list">
+                        <li>
+                            <input type="radio" id="radio01">
+                            <label for="radio01">회원</label>
+                        </li>
+                        <li>
+                            <input type="radio" id="radio02">
+                            <label for="radio02">비회원</label>
+                        </li>
+                    </ul>
+                </div>
+                <div class="write">
+                    <strong>Question.</strong>
+                    <textarea></textarea>
+                </div>
             </div>
         </div>
     </div>
@@ -19,6 +39,10 @@ export default {
 
 .question-area{
     @include layout;
+}
+
+.radio-list{
+    @include radio;
 }
 
 </style>
