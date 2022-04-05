f38215ffe7ba1095217e0c3f2cdb7ae5297f129c
annkim7@github.com <ilc7748@gmail.com>
2022-04-05 15:40:26 +0900

    submit completed

--- src/components/Detail24.vue
+++ src/components/Detail24.vue
@@ -5,7 +5,7 @@
                 <div class="write">
                     <strong>From.</strong>
                     <div>
-                        <input :value="write" @keyup="wirteTxt" type="text" name="search-input" placeholder="이름">
+                        <input v-model="array.name" @keyup="wirteTxt" type="text" name="search-input" placeholder="이름">
                     </div>
                 </div>
                 <div class="write">
@@ -13,7 +13,7 @@
                     <div>
                         <ul class="radio-list">
                             <li v-for="(regist, i) in this.register" :key="i">
-                                <input v-model="inputVal" type="radio" :id="`radio0`+i" :value="i">
+                                <input v-model="inputVal" type="radio" :id="`radio0`+i" :value="regist">
                                 <label :for="`radio0`+i">{{regist}}</label>
                             </li>
                         </ul>
@@ -22,13 +22,23 @@
                 <div class="write">
                     <strong>Question.</strong>
                     <div>
-                        <textarea :value="writeArea" @keyup="writeAreaTxt"></textarea>
+                        <textarea v-model="array.content" @keyup="writeAreaTxt" placeholder="내용을 입력하세요"></textarea>
                     </div>
                 </div>
                 <div class="submit">
-                    <span class="submit-btn">보내기</span>
+                    <span @click="submit" class="submit-btn">보내기</span>
                 </div>
             </div>
+        </div>
+    </div>
+    <div class="modal-area" v-if="this.popOpen == true">
+        <div class="modal-wrap">
+            <span @click="this.close" class="close-btn"><font-awesome-icon :icon="['fa', 'times']" /></span>
+            <div class="modal-header">
+                <h4 class="title">{{this.array.name}}</h4>
+                <span class="member">{{this.inputVal}}</span>
+            </div>
+            <div class="modal-content">{{this.array.content}}</div>
         </div>
     </div>
 </template>
@@ -42,16 +52,34 @@ export default {
             writeArea : '',
             register: ['회원', '비회원'],
             inputVal : [],
+            array : [],
+            popOpen : false,
         }
+    },
+    mounted(){
+        this.array.push({ name: this.write, register: this.inputVal, content: this.writeArea });
     },
     methods: {
         wirteTxt(event){
             this.write = event.target.value;
-            console.log(this.write);
         },
         writeAreaTxt(event){
             this.writeArea = event.target.value;
-            console.log(this.writeArea);
+        },
+        submit(){
+            if(this.write == ''){
+                alert('이름을 입력해주세요');
+            }else if(this.inputVal == ''){
+                alert('회원/비회원을 선택해주세요');
+            }else if(this.writeArea == ''){
+                alert('내용을 입력해주세요');
+            }else{
+                this.popOpen = true;
+            }
+            
+        },
+        close(){
+            this.popOpen = false;
         }
     }
 }
@@ -65,13 +93,13 @@ export default {
 }
 
 .question-box{
-    margin:-1rem auto 0;
+    margin:-0.6rem auto 0;
 }
 
 .write{
     display:flex;
     width:100%;
-    margin-top:1rem;
+    margin-top:0.3rem;
     strong{
         width: 6rem;
         margin-top:0.8rem;
@@ -80,16 +108,17 @@ export default {
         width:calc(100% - 6rem);
         input{
             width:14rem;
-            height: 2.5rem;
-            padding: 0.3rem;
+            height: 2rem;
+            margin-top:0.3rem;
+            padding: 0.8rem;
             border-radius: 0.5rem;
             box-shadow: 0 1px 4px 0 rgb(0 0 0 / 6%);
         }
         textarea{
             width:100%;
             height:10rem;
-            margin-top:0.4rem;
-            padding: 0.3rem;
+            margin-top:0.8rem;
+            padding: 0.8rem;
             border-radius: 0.5rem;
             box-shadow: 0 1px 4px 0 rgb(0 0 0 / 6%);
         }
@@ -115,11 +144,11 @@ export default {
 .radio-list{
     @include radio;
     display:flex;
-    height: 2.5rem;
-    align-items: center;
+    margin-top:0.8rem;
     li ~ li{
         margin-left:0.8rem;
     }
 }
+
 
 </style>
